16a8378fc2cc8e3ff61a856950485db2ea0af7c3
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-04-29 09:17:29 +0000

    Day 4 ques 17

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-#16Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers. Suppose the following input is supplied to the program:
+#17Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
 
-def sqrs():
-  result = [int(s)*int(s) for s in input("Enter Numbers: ").split(',') if int(s)%2!=0 ]
+# D 100
+# W 200
+# D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+
+# D 300
+# D 300
+# W 200
+# D 100
+
+
+def deposit():
+  netAmt, netWithdraw = 0,0
+  while True:
+    s = input("want to enter? type yes/no ")
+    if s == "no":
+      break
+    elif s == "yes":
+      yes = input("Enter D for deposit and W to withdraw ")
+      details = yes.split(" ")
+      if details[0] == "D":
+           netAmt+=int(details[1])
+      elif details[0] == "W":
+          netWithdraw+=int(details[1])
+      else:
+          pass
+
+        
+          
+  result = netAmt - netWithdraw
   return result
-print(sqrs())
 
+print(deposit())
+
+
+  
 
