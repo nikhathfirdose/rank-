47aa8c155d555ac4fb42b930ba5aca94c3902f02
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-04-29 09:05:06 +0000

    Day 4 ques 16

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-#15 Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+#16Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers. Suppose the following input is supplied to the program:
 
-#Suppose the following input is supplied to the program:
+def sqrs():
+  result = [int(s)*int(s) for s in input("Enter Numbers: ").split(',') if int(s)%2!=0 ]
+  return result
+print(sqrs())
 
-def getMath():
-  a = input("Enter a value to compute: ")
-  num = int(a)
-  sums = num+(num*10+num)+(num*100+num*10+num)+(num*1000+num*100+num*10+num)
-  return sums
 
-a = input()
-total,tmp = 0,str()        # initialing an integer and empty string
-for i in range(4):
-    tmp+=a  
-    print(tmp)             # concatenating 'a' to 'tmp'
-    total+=int(tmp)      # converting string type to integer type
-print(total)
