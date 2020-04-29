7493fd9a010ed3b981fbaedab9bdf76e8d2d8a17
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-04-29 08:47:16 +0000

    Day 4 ques 15

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-#14 Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+#15 Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
 
 #Suppose the following input is supplied to the program:
 
-def calcCases():
-  upper,lower =0,0
-  sentence = input("Enter your sentence: ")
-  for letter in sentence:
-      upper+=letter.isupper()
-      lower+=letter.islower()
-  result = f"Upper: {upper} \nLower: {lower}"
-  return result
+def getMath():
+  a = input("Enter a value to compute: ")
+  num = int(a)
+  sums = num+(num*10+num)+(num*100+num*10+num)+(num*1000+num*100+num*10+num)
+  return sums
 
-print(calcCases())
-
-#author
-
-# for i in word:
-#         lower+=i.islower()
-#         upper+=i.isupper()
+a = input()
+total,tmp = 0,str()        # initialing an integer and empty string
+for i in range(4):
+    tmp+=a  
+    print(tmp)             # concatenating 'a' to 'tmp'
+    total+=int(tmp)      # converting string type to integer type
+print(total)
